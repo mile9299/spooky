5ab1d5de526b68de593343030d1a7def6cda47fa
sc <s>
2022-03-17 22:03:33 +0400

    bug fix

--- scrpts/i_curl_you.sh
+++ scrpts/i_curl_you.sh
@@ -3,7 +3,7 @@ i=0
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
  do
-   if [[ "$i" -gt 5 ]]; then
+   if [[ "$i" -gt 10 ]]; then
     break
    fi
    curl --max-time 1 http://$ip;
--- scrpts/i_scan_you.sh
+++ scrpts/i_scan_you.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-	nc -z -nv 6.6.6.6 20-32000
+	nc -z -vv -n -w 1 6.6.6.6 20-15000
 	sleep 30
 done
