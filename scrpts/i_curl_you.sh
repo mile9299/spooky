1545d4fbdab069ee3681d2322e4c3c95fa77780a
sc <s>
2022-03-17 21:02:13 +0400

    gt 5

--- scrpts/i_curl_you.sh
+++ scrpts/i_curl_you.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+COUNTER=0
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
  do
