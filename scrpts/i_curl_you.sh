94eeb48af117631526f48223536f0931a767fda0
sc <s>
2022-03-17 20:48:53 +0400

    +port scan

--- scrpts/i_curl_you.sh
+++ scrpts/i_curl_you.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
- do curl http://$ip;
+ do curl http://$ip --max-time 1;
  done
--- scrpts/i_mine_you.sh
+++ scrpts/i_mine_you.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/coinbl_ips.ipset 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
- do curl http://$ip;
+ do curl http://$ip --max-time 1;
  done
 
