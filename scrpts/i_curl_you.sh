363776f12fa7c809fce893aab563c413aa6162da
sc <s>
2022-03-17 21:08:06 +0400

    bug fix

--- scrpts/i_curl_you.sh
+++ scrpts/i_curl_you.sh
@@ -3,8 +3,8 @@ COUNTER=0
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
  do
-   curl http://$ip --max-time 1;
-   let COUNTER++;
+   curl http://$ip --max-time 1
+   let COUNTER++
    if [[ $COUNTER -gt 5 ]]; then
     break
    fi
--- scrpts/i_mine_you.sh
+++ scrpts/i_mine_you.sh
@@ -3,7 +3,7 @@ COUNTER=0
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/coinbl_ips.ipset 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
  do
-   curl http://$ip --max-time 1;
+   curl http://$ip --max-time 1
    let COUNTER++
    if [[ $COUNTER -gt 5 ]]; then
     break
