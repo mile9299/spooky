6d3788c73bef252d57ce80de69a3382bc5c8e9de
sc <s>
2022-03-17 21:25:51 +0400

    bug fix

--- scrpts/i_curl_you.sh
+++ scrpts/i_curl_you.sh
@@ -3,9 +3,10 @@ COUNTER=0
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
  do
-   curl http://$ip --max-time 1
-   let COUNTER++
-   if [[ $COUNTER -gt 10 ]]; then
+   if [[ $COUNTER == '5' ]]; then
     break
    fi
+   curl http://$ip --max-time 1
+   let COUNTER++
+
  done
