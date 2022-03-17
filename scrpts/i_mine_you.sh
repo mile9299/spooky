3376288f69009b12b44cda30548cb39abe534a0f
sc <s>
2022-03-17 20:58:47 +0400

    gt 5

--- scrpts/i_mine_you.sh
+++ scrpts/i_mine_you.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+COUNTER=0
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/coinbl_ips.ipset 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
- do curl http://$ip --max-time 1;
+ do
+   curl http://$ip --max-time 1;
+   let COUNTER++
+   if [[ $COUNTER -gt 5 ]]; then
+    break
+   fi
  done
 
