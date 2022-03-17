ee2318fdace7abec7b0add4037476a97a36fd03e
sc <s>
2022-03-17 21:29:39 +0400

    bug fix

--- scrpts/i_mine_you.sh
+++ scrpts/i_mine_you.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-COUNTER=0
+i=0
 
 for ip in $(curl --compressed https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/coinbl_ips.ipset 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
  do
-   if [[ $COUNTER == '5' ]]; then
+   if [[ "$i" -gt 5 ]]; then
     break
    fi
    curl http://$ip --max-time 1
-   let COUNTER++
+   ((i++))
 
  done
 
