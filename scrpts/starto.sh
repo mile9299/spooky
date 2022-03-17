3c00ef1fd238d0c099a030c2f0673a8779c568fa
sc <s>
2022-03-17 21:44:37 +0400

    bug fix

--- scrpts/starto.sh
+++ scrpts/starto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sh ./i_curl_you.sh &
-sh ./i_mine_you.sh &
-sh ./i_scan_you.sh &
-sh ./i_drift_you.sh &
+bash ./i_curl_you.sh &
+bash ./i_mine_you.sh &
+bash ./i_scan_you.sh &
+bash ./i_drift_you.sh &
 
 echo "Startup done! Lets rumble! WHAAAA HA HA"
 
