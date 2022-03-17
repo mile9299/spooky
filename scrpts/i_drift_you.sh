b557dbf9748065056f34072afb23be194bce7861
sc <s>
2022-03-17 21:57:32 +0400

    bug fix

--- scrpts/i_drift_you.sh
+++ scrpts/i_drift_you.sh
@@ -3,6 +3,7 @@
 while true
 do
 	cp /bin/ls ./ls_drifted
+	./ls_drifted
 	sleep 10
 	rm ./ls_drifted
 done
--- scrpts/i_mine_you.sh
+++ scrpts/i_mine_you.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 i=0
 
-for ip in $(curl --compressed https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/coinbl_ips.ipset 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
+while true
  do
    if [[ "$i" -gt 5 ]]; then
     break
    fi
-   curl --max-time 1 http://$ip;
+   curl --max-time 1 http://us-east.cryptonight-hub.miningpoolhub.com;
    ((i++))
  done
 
