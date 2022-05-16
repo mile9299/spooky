7883861ec7bbf91d8e73d7fe3c90eea2652a17e8
Ubuntu <ubuntu@ip-10-0-6-49.us-west-2.compute.internal>
2022-05-16 09:45:10 +0000

    changes scan ports

--- scrpts/i_scan_you.sh
+++ scrpts/i_scan_you.sh
@@ -3,7 +3,7 @@ LOG_NAME="malicious.log"
 while true
 do
         echo "PORT SCANNING started" >> $LOG_NAME
-	nc -z -vv -n -w 1 8.8.8.8 25,80-85,443
+	nc -z -vv -n -w 1 8.8.8.8 25,80,443
 	echo "PORT SCANNING ended" >> $LOG_NAME
 
 	sleep 100
