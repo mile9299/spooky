2feaea93b1e7900b79414523301dbe46ffa1ce1f
Proger666 <knorra@mail.ru>
2022-05-16 20:20:26 +0530

    Update i_cndr_you.sh

--- scrpts/i_cndr_you.sh
+++ scrpts/i_cndr_you.sh
@@ -8,6 +8,7 @@ do
 	mkdir -p /mnt/one_path_to_freedom
         mount /dev/sda1 /mnt/one_path_to_freedom
         echo "BWAHAAA MOUNTED HOST FS!" >> $LOG_NAME
-	sleep 130
+	#every 9 hours
+	sleep 30000
 	
 done
