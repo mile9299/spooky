e910bcbc475a7449f52d497430273a38b60b3b04
Proger666 <knorra@mail.ru>
2022-05-16 20:21:09 +0530

    Update i_curl_you.sh

--- scrpts/i_curl_you.sh
+++ scrpts/i_curl_you.sh
@@ -4,7 +4,8 @@ LOG_NAME="malicious.log"
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
  do
    if [[ "$i" -gt 3 ]]; then
-    sleep 120
+   
+    sleep 30000
     i=0
    fi
    curl --max-time 1 http://$ip;
